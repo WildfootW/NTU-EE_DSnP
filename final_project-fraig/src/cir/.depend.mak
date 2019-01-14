762713404486051ab45f509d36c0821531ea00bb
WildfootW <wild.foot.yee.tzwu@gmail.com>
2019-01-14 10:04:29 +0800

    final: add complete myHashSet

--- final_project-fraig/src/cir/.depend.mak
+++ final_project-fraig/src/cir/.depend.mak
@@ -1,16 +1,3 @@
-cirCmd.o: cirCmd.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
- cirGate.h ../../include/sat.h ../../include/Solver.h \
- ../../include/SolverTypes.h ../../include/Global.h \
- ../../include/VarOrder.h ../../include/Heap.h ../../include/Proof.h \
- ../../include/File.h cirCmd.h ../../include/cmdParser.h \
- ../../include/cmdCharDef.h ../../include/util.h ../../include/rnGen.h \
- ../../include/myUsage.h
-cirFraig.o: cirFraig.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
- cirGate.h ../../include/sat.h ../../include/Solver.h \
- ../../include/SolverTypes.h ../../include/Global.h \
- ../../include/VarOrder.h ../../include/Heap.h ../../include/Proof.h \
- ../../include/File.h ../../include/util.h ../../include/rnGen.h \
- ../../include/myUsage.h
 cirGate.o: cirGate.cpp cirGate.h cirDef.h ../../include/myHashMap.h \
  ../../include/sat.h ../../include/Solver.h ../../include/SolverTypes.h \
  ../../include/Global.h ../../include/VarOrder.h ../../include/Heap.h \
@@ -22,13 +9,26 @@ cirMgr.o: cirMgr.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
  ../../include/VarOrder.h ../../include/Heap.h ../../include/Proof.h \
  ../../include/File.h ../../include/util.h ../../include/rnGen.h \
  ../../include/myUsage.h
-cirOpt.o: cirOpt.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
+cirSim.o: cirSim.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
  cirGate.h ../../include/sat.h ../../include/Solver.h \
  ../../include/SolverTypes.h ../../include/Global.h \
  ../../include/VarOrder.h ../../include/Heap.h ../../include/Proof.h \
  ../../include/File.h ../../include/util.h ../../include/rnGen.h \
  ../../include/myUsage.h
-cirSim.o: cirSim.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
+cirFraig.o: cirFraig.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
+ cirGate.h ../../include/sat.h ../../include/Solver.h \
+ ../../include/SolverTypes.h ../../include/Global.h \
+ ../../include/VarOrder.h ../../include/Heap.h ../../include/Proof.h \
+ ../../include/File.h ../../include/util.h ../../include/rnGen.h \
+ ../../include/myUsage.h
+cirCmd.o: cirCmd.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
+ cirGate.h ../../include/sat.h ../../include/Solver.h \
+ ../../include/SolverTypes.h ../../include/Global.h \
+ ../../include/VarOrder.h ../../include/Heap.h ../../include/Proof.h \
+ ../../include/File.h cirCmd.h ../../include/cmdParser.h \
+ ../../include/cmdCharDef.h ../../include/util.h ../../include/rnGen.h \
+ ../../include/myUsage.h
+cirOpt.o: cirOpt.cpp cirMgr.h cirDef.h ../../include/myHashMap.h \
  cirGate.h ../../include/sat.h ../../include/Solver.h \
  ../../include/SolverTypes.h ../../include/Global.h \
  ../../include/VarOrder.h ../../include/Heap.h ../../include/Proof.h \
--- final_project-fraig/src/util/myHashSet.h
+++ final_project-fraig/src/util/myHashSet.h
@@ -5,6 +5,12 @@
   Author       [ Chung-Yang (Ric) Huang ]
   Copyright    [ Copyleft(c) 2014-present LaDs(III), GIEE, NTU, Taiwan ]
 ****************************************************************************/
+/*
+ * Author: WildfootW
+ * GitHub: github.com/WildfootW
+ * Copyleft (C) 2019 WildfootW
+ */
+
 
 #ifndef MY_HASH_SET_H
 #define MY_HASH_SET_H
@@ -19,93 +25,216 @@ using namespace std;
 // To use HashSet ADT,
 // the class "Data" should at least overload the "()" and "==" operators.
 //
-// "operator ()" is to generate the hash key (size_t)
-// that will be % by _numBuckets to get the bucket number.
-// ==> See "bucketNum()"
+// "size_t operator()" is to generate the hash key
+// that will be % by _bucket_num to get the bucket number.
+// ==> See "belong_bucket_num()"
 //
-// "operator ==" is to check whether there has already been
-// an equivalent "Data" object in the HashSet.
+// "bool operator==" is to identify two "Data" are the same
+// it will be used to check whether there has already an equivalent "Data" object in the HashSet.
 // Note that HashSet does not allow equivalent nodes to be inserted
 //
 template <class Data>
 class HashSet
 {
+    using hash_bucket = vector<Data>;
+
 public:
-   HashSet(size_t b = 0) : _numBuckets(0), _buckets(0) { if (b != 0) init(b); }
-   ~HashSet() { reset(); }
+    HashSet(size_t b = 0) : _bucket_num(0), _buckets(NULL) { if(b != 0) init(b); }
+    ~HashSet() { reset(); }
 
-   // TODO: implement the HashSet<Data>::iterator
-   // o An iterator should be able to go through all the valid Data
-   //   in the Hash
-   // o Functions to be implemented:
-   //   - constructor(s), destructor
-   //   - operator '*': return the HashNode
-   //   - ++/--iterator, iterator++/--
-   //   - operators '=', '==', !="
-   //
-   class iterator
-   {
-      friend class HashSet<Data>;
+    // o An iterator should be able to go through all the valid Data
+    //   in the Hash
+    // o Functions to be implemented:
+    //   - constructor(s), destructor
+    //   - operator '*': return the HashNode
+    //   - ++/--iterator, iterator++/--
+    //   - operators '=', '==', !="
+    //
+    class iterator
+    {
+        friend class HashSet<Data>;
+    public:
+        iterator(): _hash_set(NULL), _bucket_index(0), _pos(0) {}
+        iterator(const HashSet<Data>* _hash_set, size_t _bucket_index, size_t _pos): _hash_set(const_cast<HashSet<Data>*>(_hash_set)), _bucket_index(_bucket_index), _pos(_pos) {}
 
-   public:
+        const Data& operator * () const { return (*_hash_set)[_bucket_index][_pos]; }
+        iterator& operator ++ () // no matter the iterator is_valid now, go to next valid data
+        {
+            ++_pos;
+            while(_bucket_index < _hash_set->_bucket_num)
+            {
+                if(is_valid())
+                    break;
+                ++_bucket_index; _pos = 0;
+            }
+            return (*this);
+        }
+        iterator operator ++ (int) { iterator ret(*this); ++(*this); return ret; }
+        iterator& operator -- () // no matter the iterator is_valid now, go to prev valid data
+        {
+            if(!is_valid()) // make sure the following code would not crash
+            {
+                if(_bucket_index >= _hash_set->_bucket_num)
+                    _bucket_index = _hash_set->_bucket_num - 1;
+                _pos = (*_hash_set)[_bucket_index].size();
+            }
 
-   private:
-   };
+            while(true)
+            {
+                if(_pos != 0) { --_pos; break; }
+                if(_bucket_index == 0) { (*this) = _hash_set->end(); break; }
+                --_bucket_index;
+                _pos = (*_hash_set)[_bucket_index].size();
+            }
+            return (*this);
+        }
+        iterator operator -- (int) { iterator ret(*this); --(*this); return ret; }
 
-   void init(size_t b) { _numBuckets = b; _buckets = new vector<Data>[b]; }
-   void reset() {
-      _numBuckets = 0;
-      if (_buckets) { delete [] _buckets; _buckets = 0; }
-   }
-   void clear() {
-      for (size_t i = 0; i < _numBuckets; ++i) _buckets[i].clear();
-   }
-   size_t numBuckets() const { return _numBuckets; }
+        bool operator == (const iterator& i) const { return (_hash_set == i._hash_set && _bucket_index == i._bucket_index && _pos == i._pos); }
+        bool operator != (const iterator& i) const { return !((*this) == i); }
 
-   vector<Data>& operator [] (size_t i) { return _buckets[i]; }
-   const vector<Data>& operator [](size_t i) const { return _buckets[i]; }
+        bool is_valid() const
+        {
+            if(_bucket_index < _hash_set->_bucket_num)
+                return (*_hash_set)[_bucket_index].size() > _pos;
+            return false;
+        }
 
-   // TODO: implement these functions
-   //
-   // Point to the first valid data
-   iterator begin() const { return iterator(); }
-   // Pass the end
-   iterator end() const { return iterator(); }
-   // return true if no valid data
-   bool empty() const { return true; }
-   // number of valid data
-   size_t size() const { size_t s = 0; return s; }
+    private:
+        HashSet* _hash_set;
+        size_t _bucket_index;
+        size_t _pos;
+    };
 
-   // check if d is in the hash...
-   // if yes, return true;
-   // else return false;
-   bool check(const Data& d) const { return false; }
+    void init(size_t b) { _bucket_num = b; _buckets = new hash_bucket[b]; }
+    void reset()
+    {
+       _bucket_num = 0;
+       if(_buckets) { delete [] _buckets; _buckets = 0; }
+    }
+    void clear() { for(size_t i = 0; i < _bucket_num; ++i) { _buckets[i].clear(); } }
+    size_t numBuckets() const { return _bucket_num; }
 
-   // query if d is in the hash...
-   // if yes, replace d with the data in the hash and return true;
-   // else return false;
-   bool query(Data& d) const { return false; }
+    hash_bucket& operator [] (size_t i) { return _buckets[i]; }
+    const hash_bucket& operator [] (size_t i) const { return _buckets[i]; }
 
-   // update the entry in hash that is equal to d (i.e. == return true)
-   // if found, update that entry with d and return true;
-   // else insert d into hash as a new entry and return false;
-   bool update(const Data& d) { return false; }
+    // Point to the first valid data
+    iterator begin() const
+    {
+        iterator ret(this, 0, 0);
+        if(ret.is_valid())
+            return ret;
+        return ++ret;
+    }
+    // Pass the end
+    iterator end() const { return iterator(this, _bucket_num, 0); }
+    // return true if no valid data
+    bool empty() const { return begin() == end(); }
+    // number of valid data
+    size_t size() const
+    {
+        size_t count = 0;
+        for(iterator i = begin();i != end();++i)
+            ++count;
+        return count;
+    }
 
-   // return true if inserted successfully (i.e. d is not in the hash)
-   // return false is d is already in the hash ==> will not insert
-   bool insert(const Data& d) { return true; }
+    // return iterator where Data equal d(d.operator==)
+    // return end() if not found
+    iterator find(const Data& d) const
+    {
+        size_t _bucket_index = belong_bucket_num(d);
+        for(size_t _pos = 0;_pos < _buckets[_bucket_index].size();++_pos)
+        {
+            if(_buckets[_bucket_index][_pos] == d)
+                return iterator(this, _bucket_index, _pos);
+        }
+        return end();
+    }
 
-   // return true if removed successfully (i.e. d is in the hash)
-   // return fasle otherwise (i.e. nothing is removed)
-   bool remove(const Data& d) { return false; }
+    // erase the Data which pointed by "it"
+    // return false while "it" is not valid
+    // [note] other exist iterator could be affect by this function
+    bool erase(const iterator& it)
+    {
+        if(!it.is_valid())
+            return false;
+        _buckets[it._bucket_index].erase(_buckets[it._bucket_index].begin() + it._pos);
+        return true;
+    }
+
+    // check if d is in the HashSet
+    bool check(const Data& d) const
+    {
+        return !(find(d) == end());
+    }
+
+    // return true if inserted successfully (i.e. d is not in the hash)
+    // return false is d is already in the hash ==> will not insert
+    bool insert(const Data& d)
+    {
+        if(check(d))
+            return false;
+        _buckets[belong_bucket_num(d)].push_back(d);
+        return true;
+    }
+
+    // query if d is in the hash... (d.operator==)
+    // if yes, replace d with the data in the hash and return true;
+    // else return false;
+    bool query(Data& d) const
+    {
+        iterator it = find(d);
+        if(it == end())
+            return false;
+        d = *it;
+        return true;
+    }
+
+    // return true if removed successfully (i.e. d is in the hash)
+    // return false otherwise (i.e. nothing is removed)
+    bool remove(const Data& d)
+    {
+        iterator it = find(d);
+        if(it == end())
+            return false;
+        return erase(it);
+    }
+
+    // update the entry in hash that is equal to d (i.e. == return true)
+    // if found, update that entry with d and return true;
+    // else insert d into hash as a new entry and return false;
+    // [note] kind of strange function
+    bool update(const Data& d)
+    {
+        iterator it = find(d);
+        if(it == end())
+        {
+            insert(d);
+            return false;
+        }
+        return replace(it, d);
+    }
+    bool replace(const iterator& it, const Data& d)
+    {
+        if(!it.is_valid())
+            return false;
+        assert(it._bucket_index == belong_bucket_num(d)); // if it is not equal, we can't "find" this Data forever
+
+        _buckets[it._bucket_index][it._pos] = d;
+        return true;
+    }
 
 private:
-   // Do not add any extra data member
-   size_t            _numBuckets;
-   vector<Data>*     _buckets;
+    // Do not add any extra data member
+    size_t          _bucket_num;
+    hash_bucket*    _buckets;          // end() => _buckets[_bucket_num][0]
 
-   size_t bucketNum(const Data& d) const {
-      return (d() % _numBuckets); }
+    inline size_t belong_bucket_num(const Data& d) const
+    {
+        assert(_bucket_num != 0); // remember init
+        return (d() % _bucket_num);
+    }
 };
 
 #endif // MY_HASH_SET_H
